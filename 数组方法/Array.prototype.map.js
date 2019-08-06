d02e09e56e1a66194ffbc7c4edebf58348b657fd
gaochao <gaochao@oneapm.com>
2019-08-06 22:45:19 +0800

    map

--- 数组方法/Array.prototype.map.js
+++ 数组方法/Array.prototype.map.js
@@ -1,3 +1,18 @@
 let ary = [1,3,4,5,6,7]
 const map1 = ary.map(x => x * 2);
 console.log(map1)
+
+
+let arr = [
+    {key:1,value:10},
+    {key:1,value:10},
+    {key:1,value:10}
+]
+
+let reformattedArray = arr.map(function(obj){
+    let rObj = {};
+    rObj[obj.key] = obj.value
+    return rObj;
+})
+
+console.log(reformattedArray)
