4664c84fd1f1a0069fc8a2c3006baa10001360e0
高超 <cgao@kaikeba.com>
2021-07-29 14:27:45 +0800

    feat: hooks

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,38 +1,86 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-
-let hookState = [] // 存放所有状态
+import ReactDOM from 'react-dom'
+let hookState = [] //存放所有状态的数组，一进来就调用了两次，所以是分别赋值
 let hookIndex = 0 // 索引
-function useState (initialState) {
+function useState (initialState){
   hookState[hookIndex] = hookState[hookIndex] || initialState
-  let currentIndex = hookIndex
-  function setState (newState) {
+  let currentIndex = hookIndex  // 利用闭包的机制, 外层hook会多次渲染 但是我只改变我点击的值得
+  function setState (newState){
+
     hookState[currentIndex] = newState
     render()
   }
   return [hookState[hookIndex++], setState]
 }
+/**
+ * useMemo useCallback
+ * 1. 优化，有一个依赖项，如果依赖的值变化了就会更新，如果依赖项没有变化就不更新
+ */
 
-function Counter () {
-  let [number1, setNumber1] = useState(0);
-  let [number2, setNumber2] = useState(0);
-
+let Child = ({data, onButtonClick}) =>{
+  console.log('child render')
   return(
-    <div>
-      <p>{number1}</p>
-      <button onClick={()=> setNumber1(number1+1)}>+</button>
-      <button onClick={()=> setNumber1(number1-1)}>-</button>
-      <p>{number2}</p>
-      <button onClick={()=> setNumber2(number2+1)}>+</button>
-      <button onClick={()=> setNumber2(number2-1)}>-</button>
-    </div>
+    <button onClick={()=>onButtonClick()}>{data.number}</button>
   )
 }
 
-function render () {
-  hookIndex = 0
-  ReactDOM.render(<Counter/>, document.getElementById("root"))
+// Child = React.memo(Child);
+
+
+function useCallback (callback, depent) {
+  if(hookState[hookIndex]){ // 是否是第一次渲染？不是第一次渲染 
+    let [lastCallback, lastDepent] = hookState[hookIndex]
+     //判断依赖有没有变化
+     let same = depent.every((item, index)=> item === lastDepent[index])
+     if(same){ // 如果有任意一项满足条件 证明没有变化
+      hookIndex++
+      return lastCallback
+     } else { // 就是有变化
+      hookState[hookIndex++] = [callback, depent]
+      return callback
+     }
+  } else { // 是第一次渲染
+    hookState[hookIndex++] = [callback, depent] //hookIndex = 3
+    return callback
+  }
+}
+
+function useMemo (factory, depent) {
+  if(hookState[hookIndex]){ // 是否是第一次渲染？不是第一次渲染 
+    let [lastMemo, lastDepent] = hookState[hookIndex]
+     //判断依赖有没有变化
+     let same = depent.every((item, index)=> item === lastDepent[index])
+     if(same){ // 如果有所有项没有满足条件 证明没有变化
+      hookIndex++
+      return lastMemo
+     } else { // 就是有变化
+      let newMemo = factory()
+      hookState[hookIndex++] = [newMemo, depent]
+      return newMemo
+     }
+  } else { // 是第一次渲染
+    let newMemo = factory()
+    hookState[hookIndex++] = [newMemo, depent]
+    return newMemo
+  }
+}
+
+let App = () =>{
+  let [number, setNumber] = useState(0)
+  let [name, setName] = useState('hooks')
+  let data = useMemo(()=>({number}), [number])
+  let addClick = useCallback(()=>setNumber(number+1), [number]) // 优化  检查属性是否变化
+  return(
+    <>
+      <input value={name} onChange={(e)=> setName(e.target.value)}/>
+      {
+        React.useMemo(()=> <Child data={data} onButtonClick={addClick}/>, [number])
+      }
+    </>
+  )
+}
+function render() {
+  hookIndex = 0 // 每次渲染的时候需要清空下标
+  ReactDOM.render(<App />, document.getElementById('root'))
 }
 render()
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,78 +1,17 @@
-let path = require('path');   //  自带路径模块
-let HtmlWabpackPlugin = require('html-webpack-plugin');   // 处理index插件
-let PostcssPxToViewport = require('postcss-px-to-viewport');
 
-//  手动选择配置文件命令   npx webpack --config webpack.config.my.js
-module.exports = {
-    devServer:{ //开发的配置
-        port:3001,
-        progress:true,
-        open:true,
-        contentBase:'./dist'
-    },
-    mode:'development',   // 模式，默认两种 production 生产环境  development开发环境
-    entry: './src/index.js', // 入口
-    output:{
-        filename:'bundle.js', // 打包后的文件名  可以加入[hash:8]戳
-        path:path.resolve(__dirname,'dist')  //  会自动转为绝对路径
-    },
-    plugins:[   // 数组  放着所有webpack插件
-        new HtmlWabpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-        })
-        // new PostcssPxToViewport({
-        //     viewportWidth: 750, // 视窗的宽度，对应的是我们设计稿的宽度，一般是750
-        //     viewportHeight: 1334, // 视窗的高度，根据750设备的宽度来指定，一般指定1334，也可以不配置
-        //     unitPrecision: 3,// 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
-        //     viewportUnit: 'vw', // 指定需要转换成的视窗单位，建议使用vw
-        //     selectorBlackList: ['.ignore', '.hairlines'], // 指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
-        //     minPixelValue: 1, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
-        //     mediaQuery: false // 允许在媒体查询中转换`px`
-        // })
-        
-    ],
-    module:{ // 模块，处理css模块
-        rules:[ // 规则  css-loader  负责解析@import这种语法的
-            // style-loder 它是吧css插入到head的标签中
-            //loader的特点  希望单一   用法，  字符串只用一个loader，多个loader需要【】
-            // loader的顺序 默认是从右向左执行,从下到上执行
-            // loader还可以写成对象方式
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use:[
-                    
-                    {
-                        loader:'style-loader',
-                        // options:{
-                        //     insertAt:'top' //  文件位置
-                        // }
-                    },
-                    {
-                        loader:'css-loader',
-                    }
-                    
-                    
-                ]
-            },
-            // {
-            //     test: /\.stylus$/,
-            //     use:[
-            //         {
-            //             loader:'style-loader',
-            //             // options:{
-            //             //     insertAt:'top' //  文件位置
-            //             // }
-            //         },
-            //         'css-loader',
-            //         'postcss-loader',
-            //         'stylus-loader'
-            //     ]
-            // }
-            
-            
 
-        ]
-    }
+module.export  = {
+  enter: './src/index.js',
+  output:{
+    filename: 'main.js',
+    path: 'dist'
+  },
+  module:{
+    rule:[
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  }
 }
